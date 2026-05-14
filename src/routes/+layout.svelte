f6f035f3a7986cc2e3748e62ad98cb79a1b1cc92
hyeyseo <hyeyseo@gmarket.com>
2026-05-14 09:24:51 +0900

    vercel analytics 추가

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.png';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 	let { children } = $props();
 </script>
